879d44bc6fc549e84cb43fbe755e3ae93962e994
RiccardoLoPresti <riccardolopresti35@gmail.com>
2023-01-26 15:29:39 +0100

    [milestone 3] + [milestone 4]

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return{
+      baseUrl:'http://127.0.0.1:8000/api/projects',
+      projects:[]
+    }
+  },
+  methods:{
+    getApi(){
+      axios.get(this.baseUrl)
+        .then(result => {
+          this.projects = result.data.projects;
+          console.log(this.projects)
+        })
+    }
+  },
+  mounted(){
+    this.getApi(this.baseUrl)
+  }
 }
 </script>
 
